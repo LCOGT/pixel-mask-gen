b4f3a81648b9543c12509929e76b9f64e5b7bddd
Raleigh Littles <rlittles@lco.global>
2018-06-18 16:02:12 -0700

    Seems to coo correctly generate a bad pixel mask? since a valid file is now produced and viewable in DS9

--- script.py
+++ script.py
@@ -6,9 +6,23 @@ import os
 import yaml
 import time
 import datetime
+import logging
+import sys
+import functools
 import pdb
 
+def setup_custom_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    logger = logging.getLogger(name)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    #logger.addHandler(handler)
 
+    return logger
 def locate_image_list():
     """
 
@@ -26,13 +40,19 @@ def locate_image_list():
         # the format is camera/date/<start> or camera/date/<end>
         image_folder = os.path.join(directory_info['top_directory'], directory_info['camera_prefix'],
                                     date_string)
-        print("image_folder", image_folder)
+        #print("image_folder", image_folder)
+        logger.info("Image folder is {}".format(image_folder))
 
         # Stores the absolute path of the images in a list
         #image_list = [os.path.abspath(file) for file in os.listdir(image_folder)]
         image_list = [os.path.abspath(os.path.join(image_folder, filename)) for filename in os.listdir(image_folder)]
-        print("image list:")
-        print(image_list)
+        #print("image list:")
+
+        if len(image_list) < 1:
+            raise ValueError('No images found -- please check that path was correct.')
+
+        else:
+            logger.info("{} images found".format(len(image_list)))
 
         return image_list
 
@@ -54,17 +74,35 @@ def extract_data_from_files(image_list, prefixes_array):
             if image_filename.endswith("{0}.fits".format(prefix)):
                 image_file = astropy.io.fits.open(image_filename)
                 image_data = image_file[0].data
-                x_size, y_size = image_data.shape
-                image_file.close()
-                print("x_size:", x_size, "y_size:", y_size, "for image: ", image_filename)
-                arr_string = prefix[0] + "_array"
-                eval(arr_string + '.append(image_data)') # example: b_array.append(image_data)
+                rows, cols = image_data.shape
+                if (rows == 0) or (cols == 0):
+                    logger.error("The image {0} has no data".format(image_filename))
+                    raise ValueError("The image you are trying to read has no data!")
 
-                # once you've found the matching prefix, stop checking for other prefixes -- at
-                # most once prefix can match
-                break
+                else:
+                    logger.info("Located image: {0} having shape: {1} by {2}".format(image_filename, rows, cols))
+                    image_file.close()
+                    #print("x_size:", x_size, "y_size:", y_size, "for image: ", image_filename)
+                    arr_string = prefix[0] + "_array"
+                    eval(arr_string + '.append(image_data)') # example: b_array.append(image_data)
+
+                    # once you've found the matching prefix, stop checking for other prefixes -- at
+                    # most once prefix can match
+                    break
 
     return (b_array, d_array, f_array) # return this in alphabetical order
+
+def filter_individual(image_array, sigma_hi, sigma_low):
+    mfiltered_array = astropy.stats.sigma_clip(data=image_array,\
+                                    sigma_lower=sigma_low,\
+                                    sigma_upper=sigma_hi)
+
+    masked_indices = numpy.transpose(numpy.ma.getmask(mfiltered_array).nonzero())
+
+    percentage_masked = (len(masked_indices) / mfiltered_array.size) * 100
+
+    return (mfiltered_array, masked_indices, percentage_masked)
+
 
 def run_median_filtering(images_arrays):
     """
@@ -85,6 +123,7 @@ def run_median_filtering(images_arrays):
         sigma_hi = statistics_info['sigma_high']
         sigma_low = statistics_info['sigma_low']
         threshold = statistics_info['threshold']
+        max_pct_removed = statistics_info['max_pct_removed']
 
     # Run sigma clipping on each image, see what points were masked
 
@@ -93,16 +132,32 @@ def run_median_filtering(images_arrays):
 
     for index_cal_array, cal_array in enumerate(images_arrays):
         for index_image_array, image_array in enumerate(cal_array):
+            """
             mfiltered_array = astropy.stats.sigma_clip(data=image_array,\
                                     sigma_lower=sigma_low,\
                                     sigma_upper=sigma_hi)
+            """
 
             # figure out the indices of the pixels that were masked for each image, and add them to
             #  the corresponding bpm collection
 
-            masked_indices = numpy.transpose(numpy.ma.getmask(mfiltered_array).nonzero())
+            #masked_indices = numpy.transpose(numpy.ma.getmask(mfiltered_array).nonzero())
 
-            print(len(masked_indices), "pixels failed sigma clipping on image #", index_image_array, "of the current type")
+            filtered_array, masked_indices, percentage_masked = filter_individual(image_array, sigma_hi, sigma_low)
+
+            #print(len(masked_indices), "pixels failed sigma clipping on image #", index_image_array, "of the current type")
+            logger.info("{0} ({1}% of total) pixels failed the sigma clipping on image #{2}".format(len(masked_indices), percentage_masked, index_image_array))
+
+            # make sure too many pixels arent getting filtered, but if they are, increase the minimum std dev that you
+            # allow
+            while (percentage_masked >= max_pct_removed):
+                logger.info("Too many pixels failed sigma clipping. Lowering sigma values from {0} to {1} (low) and from {2} to {3} (high)".format(sigma_low, sigma_low +1, sigma_hi, sigma_hi + 1))
+                sigma_hi += 1
+                sigma_low += 1
+                filtered_array, masked_indices, percentage_masked = filter_individual(image_array, sigma_hi,sigma_low)
+                logger.info("{0} ({1}% of total) pixels failed the sigma clipping on image #{2}".format(len(masked_indices), percentage_masked, index_image_array))
+
+
 
             if (index_cal_array == 0):
                 bias_bpm_collection.append(masked_indices)
@@ -155,21 +210,20 @@ def run_median_filtering(images_arrays):
     return thresholded_bias_array, thresholded_dark_array, thresholded_flat_array
 
 
-def generate_mask_from_bad_pixels(bad_pixel_location_arrays, x_dimension, y_dimension, debug=True):
+def generate_mask_from_bad_pixels(bad_pixel_location_array, x_dimension, y_dimension):
     """
-    Each array in the parameter given has the coordinates of all known-bad pixels. Use these to create 3 different masks (one for
-    each calibration type) where the masks have True for bad pixels and false otherwise
+    The array in the parameter given has the coordinates of all known-bad pixels. Use these to create a mask where the masks have True for bad pixels and false otherwise
     :param bad_pixel_location_arrays:
     :return:
     """
-    array_masks = []
-
+    """
     for index_bad_pixel_array, bad_pixel_array in enumerate(bad_pixel_location_arrays):
         masked_array = numpy.zeros((x_dimension, y_dimension), dtype=bool)
         #pdb.set_trace()
         #masked_array[bad_pixel_array] = True
         # Bad pixel array contains coordinates of all known bad pixels, so set these coordinates to true in the array
-        print(len(bad_pixel_array), "known-bad pixels found")
+        #print(len(bad_pixel_array), "known-bad pixels found")
+        logger.info("{0} ({1}% of total) bad pixels detected".format(len(bad_pixel_array), len(bad_pixel_array) / (x_dimension * y_dimension)))
         for coordinates in bad_pixel_array:
             masked_array[coordinates] = True
 
@@ -181,8 +235,42 @@ def generate_mask_from_bad_pixels(bad_pixel_location_arrays, x_dimension, y_dime
                 output.write(''.join(map(str, bad_pixel_array)))
             output.close()
 
+    """
 
-    return array_masks
+    masked_array = numpy.zeros((x_dimension, y_dimension), dtype=bool)
+    size = x_dimension * y_dimension
+    logger.info("Final count: {0} ({1}% of total) bad pixels detected.".format(len(bad_pixel_location_array), (len(bad_pixel_location_array) / size) * 100))
+    for coordinates in bad_pixel_location_array:
+        masked_array[coordinates] = True
+
+    return masked_array
+
+
+def combine_bad_pixel_locations(arrays_of_bad_pixels):
+    """
+    Given an array of 3 arrays, (where each inner array contains a list of pixels that are known bad), take the set union
+    :param array_of_masks:
+    :return:
+    """
+    for index, arr in enumerate(arrays_of_bad_pixels):
+        logger.info("{0} bad pixels were detected for calibration type #{1}".format(len(arr), index))
+        indiv_file_path = "debug/{0}_bpm.txt".format(index)
+        with open(indiv_file_path, 'w') as output:
+            output.write(''.join(map(str, arr)))
+            output.close()
+    #final_badpixellist_combined = functools.reduce(numpy.union1d, (arrays_of_bad_pixels[0], arrays_of_bad_pixels[1], arrays_of_bad_pixels[2]))
+
+    final_bad_pixel_set = set()
+    for subarray in arrays_of_bad_pixels:
+        for coords in subarray:
+            final_bad_pixel_set.add(coords)
+
+    final_file_path = 'debug/combined_bpm.txt'
+    with open(final_file_path, 'w') as output:
+        output.write(''.join(map(str, final_bad_pixel_set)))
+        output.close()
+
+    return final_bad_pixel_set
 
 
 def output_to_FITS(image_data, header_dict, filename):
@@ -200,7 +288,8 @@ def output_to_FITS(image_data, header_dict, filename):
 
     else:
         new_hdu = astropy.io.fits.PrimaryHDU(image_data.astype(int))
-        print("writing array of size", image_data.shape, "to fits file. Array contains", image_data.sum(), "bad pixels in mask")
+        #print("writing array of size", image_data.shape, "to fits file. Array contains", image_data.sum(), "bad pixels in mask")
+        logging.info("Writing array of size {0} to .fits file; array contains {1} bad pixels in mask.".format(image_data.shape, image_data.sum()))
         new_hdu_list = astropy.io.fits.HDUList([new_hdu])
 
 
@@ -247,7 +336,7 @@ def parse_config_file_date(directory_info):
             print("Error parsing the exact date given from config.yml. Reverting to yesterday's date.")
 
         finally: # Revert to yesterday's date
-            return str(datetime.today() - datetime.timedelta(days=1))
+            return (datetime.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
 
     else:
         if directory_info['date']['relative'] in VALID_RELATIVE_DATES:
@@ -263,16 +352,14 @@ def parse_config_file_date(directory_info):
 
         else:
             # TODO: Replace with logging/warning
-            print("Invalid relative date. Reverting to yesterday's date.")
-            pass
-
-
-
+            logger.info("Invalid relative date in config.yml, reverting to yesterday's date.")
+            return (datetime.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
 
 
 if __name__ == '__main__':
     # run the sequence
     print("Sequence starting now.")
+    logger = setup_custom_logger('pixel_mask_gen')
 
     image_list = locate_image_list()
     # manually hardcode prefix list for now?
@@ -280,8 +367,17 @@ if __name__ == '__main__':
     clean_bias_array, clean_dark_array, clean_flat_array = run_median_filtering([bias_array, dark_array, flat_array])
 
     # temporarily hardcode image size?
-    array_of_masks = generate_mask_from_bad_pixels([clean_bias_array, clean_dark_array, clean_flat_array], 2112, 3136)
+    #array_of_masks = generate_masks_from_bad_pixels([clean_bias_array, clean_dark_array, clean_flat_array], 2112, 3136)
+    """
     for index, mask_array in enumerate(array_of_masks):
         output_to_FITS(mask_array, {}, str(index) + '_bpm.fits')
+        
+    """
+    final_bpm_list = combine_bad_pixel_locations([clean_bias_array, clean_dark_array, clean_flat_array])
+    final_bpm_mask = generate_mask_from_bad_pixels(final_bpm_list, 2112, 3136)
+    output_to_FITS(final_bpm_mask, {}, "final_bpm.fits")
+
+
+    print("Sequence ended.")
 
 
