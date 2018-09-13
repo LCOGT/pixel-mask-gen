5545f4a4914d4a1b0adb7f317d89078673d39115
Matt Daily <mdaily@lco.global>
2018-09-13 11:57:48 -0700

    Remove filenames. Not needed.

--- src/test/test_image_processing.py
+++ src/test/test_image_processing.py
@@ -5,7 +5,6 @@ import fractions #will be going away
 
 def test_apply_bias_processing():
     hdr = fits.Header()
-    hdr['FILE'] = 'unit_test_bias.fits'
 
     test_image = np.round(np.random.normal(1, 1, (100, 100)))
     bad_pixels = np.array([[np.random.randint(100), np.random.randint(100)] for index in range(0, 10)])
@@ -20,15 +19,12 @@ def test_apply_darks_processing():
     hdr = fits.Header()
     hdr['BIASSEC'] = '[1:5, 1:5]'
     hdr['EXPTIME'] = '10'
-    hdr['FILE'] = 'unit_test_dark.fits'
-
 
     test_image = np.round(np.random.normal(30, 1, (100,100)))
     bad_pixels = np.array([[np.random.randint(100), np.random.randint(100)] for index in range(0, 10)])
     test_image[tuple(bad_pixels.T)] = 1000
 
-    dark_mask = image_processing.apply_darks_processing([fits.ImageHDU(data=test_image,
-                                                                       header=hdr)])
+    dark_mask = image_processing.apply_darks_processing([fits.ImageHDU(header=hdr, data=test_image)])
 
     assert np.shape(dark_mask) == np.shape(test_image)
     np.testing.assert_array_equal((dark_mask == 1), (test_image == 1000))
@@ -36,8 +32,6 @@ def test_apply_darks_processing():
 def test_apply_flats_processing():
     hdr = fits.Header()
     hdr['FILTER'] = 'w'
-    hdr['FILE'] = 'unit_test_flat.fits'
-
 
     base_flat_image = np.random.normal(22000, 100, (100, 100))
     base_flat_std = np.std(base_flat_image)
