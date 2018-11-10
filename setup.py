8f85619c1abd54325636718080351e65fe63b830
Matt Daily <mdaily@lco.global>
2018-11-09 16:25:11 -0800

    Update README with some documentation. Change console entrypoint in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='lco-bpm-maker',
       version='0.1.0',
       packages=find_packages(),
       setup_requires=['pytest-runner'],
-      install_requires=['astropy', 'lcogt-logging'],
+      install_requires=['numpy', 'astropy', 'lcogt-logging'],
       tests_require=['pytest', 'pytest-cov'],
-      entry_points={'console_scripts': ['create_bad_pixel_mask=bpm.generate_bpm:generate_bpm']})
+      entry_points={'console_scripts': ['generate_bpm=bpm.generate_bpm:generate_bpm']})
