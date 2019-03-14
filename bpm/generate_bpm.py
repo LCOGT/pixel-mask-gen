bd90751c94b64e39af34ab09934e8e89b68f36e1
Matt Daily <mdaily@lco.global>
2019-03-14 16:35:58 -0700

    Break file writing into own function. Break mask creation into own function. Slim down single- and multi-extension fits processing functions

--- bpm/generate_bpm.py
+++ bpm/generate_bpm.py
@@ -47,23 +47,25 @@ def generate_bpm():
     calibration_frames = get_calibration_frames(os.path.normpath(args.input_directory) + '/*.fits')
 
     if calibration_frames:
-        if len(set([frame[0].header['INSTRUME'] for frame in frames])) != 1:
+        if len(set([frame[0].header['INSTRUME'] for frame in calibration_frames])) != 1:
             raise RuntimeError("Got calibration frames from more than one camera. Aborting.")
 
         #separate out single and multi-extension FITS files
-        single_extension_frames = [frame[0] for frame in calibration_frames if len(frame) == 1]
-        multi_extension_frames = [frame for frame in calibration_frames if frame not in single_extension_frames]
+        multi_extension_frames = [frame for frame in calibration_frames if len(frame) > 1]
+        single_extension_frames = [frame[0] for frame in calibration_frames if frame not in multi_extension_frames]
 
         #Create BPMs!
         process_multi_extension_frames(multi_extension_frames,
                                        int(args.dark_current_threshold),
                                        int(args.flat_sigma_threshold),
-                                       ing(args.bias_sigma_threshold))
+                                       int(args.bias_sigma_threshold),
+                                       args.output_directory)
 
         process_single_extension_frames(single_extension_frames,
                                         int(args.dark_current_threshold),
                                         int(args.flat_sigma_threshold),
-                                        ing(args.bias_sigma_threshold))
+                                        int(args.bias_sigma_threshold),
+                                        args.output_directory)
     else:
         raise RuntimeError("No calibration frames could be found. Check that the directory contains calibration frames.")
 
@@ -71,12 +73,16 @@ def generate_bpm():
 def process_multi_extension_frames(frames,
                                    dark_current_threshold,
                                    flat_sigma_threshold,
-                                   bias_sigma_threshold):
+                                   bias_sigma_threshold,
+                                   output_directory):
+    """
+    Process a set of multi-extension FITS frames into bad pixel mask(s).
+    """
     if len(frames) == 0:
         return
 
     #determine number of amplifiers from image
-    n_amplifiers = len(image_utils.get_extensions_by_name(frames[0]))
+    n_amplifiers = len(image_utils.get_extensions_by_name(frames[0], 'SCI'))
 
     camera_has_no_overscan = True
     try:
@@ -101,36 +107,43 @@ def process_multi_extension_frames(frames,
             image_utils.apply_header_value_to_all_extensions(multi_extension_frames, keyword)
 
         #Treat each amplifier as a separate image
+        masks = []
         for amplifier in range(0, n_amplifiers):
             amplifier_frames = image_utils.get_sci_extensions_from_amplifier(frames_sorted, 0)
 
-            bias_level = image_processing.get_bias_level_from_frames(get_frames_of_type(amplifier_frames, 'BIAS'), 'DATASEC') if camera_has_no_overscan else None
+            combined_mask = create_final_mask(frames_sorted,
+                                              dark_current_threshold,
+                                              flat_sigma_threshold,
+                                              bias_sigma_threshold,
+                                              camera_has_no_overscan)
 
-            dark_mask = image_processing.process_dark_frames(get_frames_of_type(frames_sorted, 'DARK'), int(dark_current_threshold), bias_level)
-            bias_mask = image_processing.process_bias_frames(get_frames_of_type(frames_sorted, 'BIAS'), int(bias_sigma_threshold))
+            masks.append(combined_mask)
 
-            flats_sorted = image_utils.sort_frames_by_header_values((get_frames_of_type(frames_sorted, 'FLAT')), 'FILTER')
-            flat_masks = [image_processing.process_flat_frames(flats_sorted[filter], int(flat_sigma_threshold), bias_level) for filter in flats_sorted.keys()]
+        header = fits.Header({'OBSTYPE': 'BPM',
+                              'DAY-OBS': frames_sorted_by_binning[binning][0].header['DAY-OBS'],
+                              'CCDSUM': binning,
+                              'SITEID': frames_sorted_by_binning[binning][0].header['SITEID'],
+                              'INSTRUME': frames_sorted_by_binning[binning][0].header['INSTRUME'],
+                              'DATE-OBS': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]})
 
-            flat_masks.extend([bias_mask, dark_mask])
-            combined_mask = np.sum(np.dstack(flat_masks), axis=2) > 0
-
-        #TODO: break mask creation into separate function - this is all the same between multi and single-extension
-        #TODO: break file writing out into one function
-
+        write_bpm_to_file(masks, output_directory, header)
 
 
 def process_single_extension_frames(frames,
                                     dark_current_threshold,
                                     flat_sigma_threshold,
-                                    bias_sigma_threshold):
+                                    bias_sigma_threshold,
+                                    output_directory):
+    """
+    Process a set of single-extension FITS frames into bad pixel mask(s).
+    """
     if len(frames) == 0:
         return
 
     #check if camera has an overscan region
     camera_has_no_overscan = True
     try:
-        bias_section = image_utils.get_slices_from_header_section(frames[0][0].header['BIASSEC'])
+        bias_section = image_utils.get_slices_from_header_section(frames[0].header['BIASSEC'])
         camera_has_no_overscan = False
     except:
         logger.warn("Couldn't parse BIASSEC keyword. Using bias frames to determine camera bias level.")
@@ -141,37 +154,59 @@ def process_single_extension_frames(frames,
     for binning in frames_sorted_by_binning.keys():
         frames_sorted = frames_sorted_by_binning[binning]
 
-        bias_level  = image_processing.get_bias_level_from_frames(get_frames_of_type(frames_sorted, 'BIAS')) if camera_has_no_overscan else None
+        combined_mask = create_final_mask(frames_sorted,
+                                          dark_current_threshold,
+                                          flat_sigma_threshold,
+                                          bias_sigma_threshold,
+                                          camera_has_no_overscan)
 
-        dark_mask = image_processing.process_dark_frames(get_frames_of_type(frames_sorted, 'DARK'), int(dark_current_threshold), bias_level)
-        bias_mask = image_processing.process_bias_frames(get_frames_of_type(frames_sorted, 'BIAS'), int(bias_sigma_threshold))
+        header = fits.Header({'OBSTYPE': 'BPM',
+                              'DAY-OBS': frames_sorted_by_binning[binning][0].header['DAY-OBS'],
+                              'CCDSUM': binning,
+                              'SITEID': frames_sorted_by_binning[binning][0].header['SITEID'],
+                              'INSTRUME': frames_sorted_by_binning[binning][0].header['INSTRUME'],
+                              'DATE-OBS': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]})
 
-        flats_sorted = image_utils.sort_frames_by_header_values((get_frames_of_type(frames_sorted, 'FLAT')), 'FILTER')
-        flat_masks = [image_processing.process_flat_frames(flats_sorted[filter], int(flat_sigma_threshold), bias_level) for filter in flats_sorted.keys()]
+        write_bpm_to_file(combined_mask, output_directory, header)
 
-        flat_masks.extend([bias_mask, dark_mask])
-        combined_mask = np.sum(np.dstack(flat_masks), axis=2) > 0
 
-        #break into own function
-        today_date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%S")
-        instrument_code = frames_sorted_by_binning[binning][0].header['INSTRUME']
+def write_bpm_to_file(combined_masks, output_directory, header):
+    """
+    Write output BPM to file.
+    """
 
-        header_info = {'OBSTYPE': 'BPM',
-                       'DAY-OBS': frames_sorted_by_binning[binning][0].header['DAY-OBS'],
-                       'CCDSUM': binning,
-                       'SITEID': frames_sorted_by_binning[binning][0].header['SITEID'],
-                       'INSTRUME': instrument_code,
-                       'DATE-OBS': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]}
+    today_date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%S")
+    output_filename = os.path.join(output_directory, "bpm-{instrument}-{bin_type}-{today}.fits".format(instrument=header['INSTRUME'],
+                                                                                                       today=today_date,
+                                                                                                       bin_type=header['CCDSUM'].replace(" ", "x")))
 
-        output_filename = os.path.join(args.output_directory, "bpm-{instrument}-{bin_type}-{today}.fits".format(instrument=instrument_code,
-                                                                                                                today=today_date,
-                                                                                                                bin_type=binning.replace(" ", "x")))
+    if combined_masks.ndim != 1:
+        hdu_list = fits.HDUList([fits.ImageHDU(data=mask, header=header) for mask in combined_masks])
+    else:
         fits.writeto(filename=output_filename,
-                     data=combined_mask.astype(np.uint8),
-                     header=fits.Header(header_info),
+                     data=combined_masks.astype(np.uint8),
+                     header=header,
                      checksum=True)
 
-        logger.info("Finished processing. BPM written to {file_path}".format(file_path=output_filename))
+    logger.info("Finished processing. BPM written to {file_path}".format(file_path=output_filename))
+
+
+def create_final_mask(frames, dark_current_threshold, bias_sigma_threshold, flat_sigma_threshold, camera_has_no_overscan=True):
+    """
+    From a set of calibration frames, create a final bad pixel mask.
+    """
+    bias_level  = image_processing.get_bias_level_from_frames(get_frames_of_type(frames, 'BIAS')) if camera_has_no_overscan else None
+
+    dark_mask = image_processing.process_dark_frames(get_frames_of_type(frames, 'DARK'), int(dark_current_threshold), bias_level)
+    bias_mask = image_processing.process_bias_frames(get_frames_of_type(frames, 'BIAS'), int(bias_sigma_threshold))
+
+    flats_sorted = image_utils.sort_frames_by_header_values((get_frames_of_type(frames, 'FLAT')), 'FILTER')
+    flat_masks = [image_processing.process_flat_frames(flats_sorted[filter], int(flat_sigma_threshold), bias_level) for filter in flats_sorted.keys()]
+
+    flat_masks.extend([bias_mask, dark_mask])
+    combined_mask = np.sum(np.dstack(flat_masks), axis=2) > 0
+
+    return combined_mask
 
 
 def get_calibration_frames(path_to_frames, calibration_types=['d00', 'f00', 'b00']):
